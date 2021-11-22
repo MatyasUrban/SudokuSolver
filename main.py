1ccecf9ccc185c5c2b1b59832be9fd9586888635
matt <urb.maty@gmail.com>
2021-11-22 19:36:35 +0100

    solving the sudoku

--- main.py
+++ main.py
@@ -10,6 +10,39 @@ sudoku_board = [
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
 
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, column = find
+    for i in range(1, 10):
+        if valid(board, i, (row, column)):
+            board[row][column] = i
+            if solve(board):
+                return True
+            board[row][column] = 0
+    return False
+
+def valid(board, number, position):
+    # check row
+    for i in range(len(board[0])):
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+    # check column
+    for i in range(len(board)):
+        if board[i][position[1]] == number and position[0] != i:
+            return False
+    # check section
+    section_x = position[1] // 3
+    section_y = position[0] // 3
+    for i in range(section_y * 3, section_y * 3 + 3):
+         for j in range(section_x * 3, section_x * 3 + 3):
+             if board[i][j] == number and (i, j) != position:
+                 return False
+    return True
+
+
 
 def print_board(board):
     for i in range(len(board)):
@@ -29,3 +62,9 @@ def find_empty(board):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return i, j
+    return None
+
+print_board(sudoku_board)
+solve(sudoku_board)
+print("============")
+print_board(sudoku_board)
